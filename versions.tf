1c5da2564119a6ac30addb7605e0b20b65cd975e
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-02-06 06:40:40 -0500

    chore: update Terraform version requirement to >= 1.5.4

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.5.4"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
